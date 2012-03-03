ce908e122c4000342610cc86670aa2c30604b19e
James Hood <james.hood@gmail.com>
2012-03-02 23:07:49 -0600

    adding java and python scripts

---
+++ java/PrimeFactors.java
@@ -0,0 +1,21 @@
+package com.pillartechnology.practice;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import org.junit.Test;
+
+public class PrimeFactors {
+
+  public static List<Integer> generate(int n) {
+    return null;
+  }
+
+  @Test
+    public void testOne() {
+      assertEquals(asList(), generate(1));
+    }
+}
+      
